6ad071d8341f57faffc807fa0100bc801d568b29
buigiangnamgh <giangnamdhv@gmail.com>
2024-04-02 09:24:27 +0700

    HIS.Desktop.Plugins.DeathInformationList

---
+++ Plugins/HIS.Desktop.Plugins.DeathInformationList/GenericTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.DeathInformationList
+{
+    public class GenericTest<T>
+    {
+        public void AddIT(T item)
+        { }
+    }
+
+    public class Test
+    {
+        public static void Main()
+        {
+            GenericTest<string> textClass = new GenericTest<string>();
+            textClass.AddIT("Hello world");
+
+            GenericTest<int> numClass = new GenericTest<int>();
+            numClass.AddIT(22);
+
+            GenericTest<object> objClass = new GenericTest<object>();
+            objClass.AddIT(new object());
+        }
+    }
+}
