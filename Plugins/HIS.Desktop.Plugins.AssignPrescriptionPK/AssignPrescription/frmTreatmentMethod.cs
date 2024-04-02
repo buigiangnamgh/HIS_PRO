b27eab36798cc361c107be1dc13cf255f3e2c62c
buigiangnamgh <giangnamdhv@gmail.com>
2024-04-02 09:15:22 +0700

    chi dinh cls

---
+++ Plugins/HIS.Desktop.Plugins.AssignPrescriptionPK/AssignPrescription/frmTreatmentMethod.cs
@@ -0,0 +1,128 @@
+﻿using Inventec.Common.Logging;
+using Inventec.Desktop.Common.Controls.ValidationRule;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HIS.Desktop.Plugins.AssignPrescriptionPK.AssignPrescription
+{
+    public partial class frmTreatmentMethod : Form
+    {
+        string content;
+        Action<string> SendContent;
+        int positionHandle;
+        public frmTreatmentMethod(string content, Action<string> SendContent)
+        {
+            InitializeComponent(); 
+            try
+            {
+                string iconPath = System.IO.Path.Combine(HIS.Desktop.LocalStorage.Location.ApplicationStoreLocation.ApplicationStartupPath, System.Configuration.ConfigurationSettings.AppSettings["Inventec.Desktop.Icon"]);
+                this.Icon = Icon.ExtractAssociatedIcon(iconPath);
+                this.content = content;
+                this.SendContent = SendContent;
+            }
+            catch (Exception ex)
+            {
+                LogSystem.Warn(ex);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (!dxValidationProvider1.Validate())
+                    return;
+                SendContent(mem.Text.Trim());
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Error(ex);
+            }
+
+        }  
+        //Disable close button
+        private const int CP_DISABLE_CLOSE_BUTTON = 0x200;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle = cp.ClassStyle | CP_DISABLE_CLOSE_BUTTON;
+                return cp;
+            }
+        }
+
+        private void dxValidationProvider1_ValidationFailed(object sender, DevExpress.XtraEditors.DXErrorProvider.ValidationFailedEventArgs e)
+        {
+            try
+            {
+                DevExpress.XtraEditors.BaseEdit edit = e.InvalidControl as DevExpress.XtraEditors.BaseEdit;
+                if (edit == null) return;
+
+                DevExpress.XtraEditors.ViewInfo.BaseEditViewInfo viewInfo = edit.GetViewInfo() as DevExpress.XtraEditors.ViewInfo.BaseEditViewInfo;
+                if (viewInfo == null) return;
+
+                if (positionHandle == -1)
+                {
+                    positionHandle = edit.TabIndex;
+                    if (edit.Visible)
+                    {
+                        edit.SelectAll();
+                        edit.Focus();
+                    }
+                }
+                if (positionHandle > edit.TabIndex)
+                {
+                    positionHandle = edit.TabIndex;
+                    if (edit.Visible)
+                    {
+                        edit.SelectAll();
+                        edit.Focus();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Error(ex);
+            }
+        }
+
+        private void frmTreatmentMethod_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                mem.Text = content;
+                ValidateForm();
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Error(ex);
+            }
+
+        }
+        private void ValidateForm()
+        {
+            ControlMaxLengthValidationRule validate = new ControlMaxLengthValidationRule();
+            validate.editor = mem;
+            validate.maxLength = 4000;
+            validate.IsRequired = true;
+            validate.ErrorType = DevExpress.XtraEditors.DXErrorProvider.ErrorType.Warning;
+            this.dxValidationProvider1.SetValidationRule(mem, validate);
+        }
+
+        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            btnSave.PerformClick();
+        }
+    }
+}
