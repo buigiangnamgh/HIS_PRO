64cb3525581968a9b956deffa1120ec64276be45
buigiangnamgh <giangnamdhv@gmail.com>
2024-08-29 13:25:05 +0700

    Xử lý dịch vụ - chặn khi tài khoản chưa nhập CCHN

---
+++ Plugins/HIS.Desktop.Plugins.ExecuteRoom/ADO/HIS_SERVICE_SPEC.cs
@@ -0,0 +1,28 @@
+﻿using MOS.EFMODEL.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.ExecuteRoom.ADO
+{
+    class HIS_SERVICE_SPEC
+    {
+
+        public string APP_CREATOR { get; set; }
+        public string APP_MODIFIER { get; set; }
+        public long? CREATE_TIME { get; set; }
+        public string CREATOR { get; set; }
+        public string GROUP_CODE { get; set; }
+        public virtual HIS_SERVICE HIS_SERVICE { get; set; }
+        public virtual HIS_SPECIALITY HIS_SPECIALITY { get; set; }
+        public long ID { get; set; }
+        public short? IS_ACTIVE { get; set; }
+        public short? IS_DELETE { get; set; }
+        public string MODIFIER { get; set; }
+        public long? MODIFY_TIME { get; set; }
+        public long SERVICE_ID { get; set; }
+        public long SPECIALITY_ID { get; set; }
+    }
+}
---
+++ Plugins/HIS.Desktop.Plugins.ExecuteRoom/ADO/HisServiceSpecFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Desktop.Plugins.ExecuteRoom.ADO
+{
+    class HisServiceSpecFilter
+    {
+
+        public long? SERVICE_ID { get; set; }
+        public List<long> SERVICE_IDs { get; set; }
+        public long? SPECIALITY_ID { get; set; }
+        public List<long> SPECIALITY_IDs { get; set; }
+    }
+}
