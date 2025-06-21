237203b408912d533ed79a1e729ab54b01abb42b
buigiangnamgh <giangnamdhv@gmail.com>
2025-06-21 21:59:30 +0700

    FingerPrint

---
+++ SHAREUTIL/Inventec.FingerPrint/SignViewInputADO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventec.FingerPrint
+{
+    public class SignViewInputADO
+    {
+        public string DriverName { get; set; }
+        
+        public Action<Bitmap> ActGetSignImageFile { get; set; }
+
+        public Action<String> ActSelectDevice { get; set; }
+    }
+}
---
+++ SHAREUTIL/Inventec.FingerPrint/frmConnectFingerPrint.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using libzkfpcsharp;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.IO;
+using Sample;
+using Inventec.FingerPrint;
+
+namespace Inventec.FingerPrint
+{
+    public partial class frmConnectFingerPrint : Form
+    {
+        IntPtr mDevHandle = IntPtr.Zero;
+        IntPtr mDBHandle = IntPtr.Zero;
+        IntPtr FormHandle = IntPtr.Zero;
+        bool bIsTimeToDie = false;
+        bool IsRegister = false;
+        bool bIdentify = true;
+        byte[] FPBuffer;
+        int RegisterCount = 0;
+        const int REGISTER_FINGER_COUNT = 3;
+
+        byte[][] RegTmps = new byte[3][];
+        byte[] RegTmp = new byte[2048];
+        byte[] CapTmp = new byte[2048];
+        int cbCapTmp = 2048;
+        int cbRegTmp = 0;
+        int iFid = 1;
+        private Action<Bitmap> actGetSignImageFile;
+        private Action<string> actSelectDevice;
+        private string deviceDefault = "";
+
+        private int mfpWidth = 0;
+        private int mfpHeight = 0;
+
+        const int MESSAGE_CAPTURED_OK = 0x0400 + 6;
+
+        [DllImport("user32.dll", EntryPoint = "SendMessageA")]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        public frmConnectFingerPrint(SignViewInputADO signViewInputADO)
+        {
+            if (signViewInputADO != null)
+            {
+                actGetSignImageFile = signViewInputADO.ActGetSignImageFile;
+                actSelectDevice = signViewInputADO.ActSelectDevice;
+                deviceDefault = signViewInputADO.DriverName;
+            }
+            InitializeComponent();
+        }
+
+        private void bnInit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmbIdx.Items.Clear();
+                int ret = zkfperrdef.ZKFP_ERR_OK;
+                if ((ret = zkfp2.Init()) == zkfperrdef.ZKFP_ERR_OK)
+                {
+                    int nCount = zkfp2.GetDeviceCount();
+                    if (nCount > 0)
+                    {
+                        for (int i = 0; i < nCount; i++)
+                        {
+                            cmbIdx.Items.Add(i.ToString());
+                        }
+                        cmbIdx.SelectedIndex = 0;
+                        Inventec.Common.Logging.LogSystem.Debug("frmConnectFingerPrint deviceDefault: " + deviceDefault);
+                        bnInit.Enabled = false;
+                        bnFree.Enabled = true;
+                        bnOpen.Enabled = true;
+                    }
+                    else
+                    {
+                        zkfp2.Terminate();
+                        MessageBox.Show("No device connected!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Initialize fail, ret=" + ret + " !");
+                }
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
+        private void bnFree_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                zkfp2.Terminate();
+                cbRegTmp = 0;
+                bnInit.Enabled = true;
+                bnFree.Enabled = false;
+                bnOpen.Enabled = false;
+                bnClose.Enabled = false;
+                bnEnroll.Enabled = false;
+                bnVerify.Enabled = false;
+                bnIdentify.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+
+        }
+
+        private void bnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                int ret = zkfp.ZKFP_ERR_OK;
+                if (IntPtr.Zero == (mDevHandle = zkfp2.OpenDevice(cmbIdx.SelectedIndex)))
+                {
+                    MessageBox.Show("OpenDevice fail");
+                    return;
+                }
+                if (IntPtr.Zero == (mDBHandle = zkfp2.DBInit()))
+                {
+                    MessageBox.Show("Init DB fail");
+                    zkfp2.CloseDevice(mDevHandle);
+                    mDevHandle = IntPtr.Zero;
+                    return;
+                }
+                bnInit.Enabled = false;
+                bnFree.Enabled = true;
+                bnOpen.Enabled = false;
+                bnClose.Enabled = true;
+                bnEnroll.Enabled = true;
+                bnVerify.Enabled = true;
+                bnIdentify.Enabled = true;
+                RegisterCount = 0;
+                cbRegTmp = 0;
+                iFid = 1;
+                for (int i = 0; i < 3; i++)
+                {
+                    RegTmps[i] = new byte[2048];
+                }
+                byte[] paramValue = new byte[4];
+                int size = 4;
+                zkfp2.GetParameters(mDevHandle, 1, paramValue, ref size);
+                zkfp2.ByteArray2Int(paramValue, ref mfpWidth);
+
+                size = 4;
+                zkfp2.GetParameters(mDevHandle, 2, paramValue, ref size);
+                zkfp2.ByteArray2Int(paramValue, ref mfpHeight);
+
+                FPBuffer = new byte[mfpWidth * mfpHeight];
+
+                Thread captureThread = new Thread(new ThreadStart(DoCapture));
+                captureThread.IsBackground = true;
+                captureThread.Start();
+                bIsTimeToDie = false;
+                textRes.Text = "Open succ";
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
+
+        private void DoCapture()
+        {
+            try
+            {
+
+                while (!bIsTimeToDie)
+                {
+                    cbCapTmp = 2048;
+                    int ret = zkfp2.AcquireFingerprint(mDevHandle, FPBuffer, CapTmp, ref cbCapTmp);
+                    if (ret == zkfp.ZKFP_ERR_OK)
+                    {
+                        SendMessage(FormHandle, MESSAGE_CAPTURED_OK, IntPtr.Zero, IntPtr.Zero);
+                    }
+                    Thread.Sleep(200);
+                }
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
+        protected override void DefWndProc(ref Message m)
+        {
+            try
+            {
+
+
+                switch (m.Msg)
+                {
+                    case MESSAGE_CAPTURED_OK:
+                        {
+                            MemoryStream ms = new MemoryStream();
+                            BitmapFormat.GetBitmap(FPBuffer, mfpWidth, mfpHeight, ref ms);
+                            Bitmap bmp = new Bitmap(ms);
+                            this.picFPImg.Image = bmp;
+                            //File.WriteAllBytes("C:\\MyFile2.Png", image);
+                            if (actGetSignImageFile != null && bmp != null && bmp.Width > 0)
+                            {
+                                actGetSignImageFile(bmp);
+                            }
+                                if (IsRegister)
+                            {
+                                int ret = zkfp.ZKFP_ERR_OK;
+                                int fid = 0, score = 0;
+                                ret = zkfp2.DBIdentify(mDBHandle, CapTmp, ref fid, ref score);
+                                if (zkfp.ZKFP_ERR_OK == ret)
+                                {
+                                    textRes.Text = "This finger was already register by " + fid + "!";
+                                    return;
+                                }
+                                if (RegisterCount > 0 && zkfp2.DBMatch(mDBHandle, CapTmp, RegTmps[RegisterCount - 1]) <= 0)
+                                {
+                                    textRes.Text = "Please press the same finger 3 times for the enrollment";
+                                    return;
+                                }
+                                Array.Copy(CapTmp, RegTmps[RegisterCount], cbCapTmp);
+                                String strBase64 = zkfp2.BlobToBase64(CapTmp, cbCapTmp);
+                                byte[] blob = zkfp2.Base64ToBlob(strBase64);
+                                RegisterCount++;
+                                if (RegisterCount >= REGISTER_FINGER_COUNT)
+                                {
+                                    RegisterCount = 0;
+                                    if (zkfp.ZKFP_ERR_OK == (ret = zkfp2.DBMerge(mDBHandle, RegTmps[0], RegTmps[1], RegTmps[2], RegTmp, ref cbRegTmp)) &&
+                                           zkfp.ZKFP_ERR_OK == (ret = zkfp2.DBAdd(mDBHandle, iFid, RegTmp)))
+                                    {
+                                        iFid++;
+                                        textRes.Text = "enroll succ";
+                                    }
+                                    else
+                                    {
+                                        textRes.Text = "enroll fail, error code=" + ret;
+                                    }
+                                    IsRegister = false;
+                                    return;
+                                }
+                                else
+                                {
+                                    textRes.Text = "You need to press the " + (REGISTER_FINGER_COUNT - RegisterCount) + " times fingerprint";
+                                }
+                            }
+                            else
+                            {
+                                if (cbRegTmp <= 0)
+                                {
+                                    textRes.Text = "Please register your finger first!";
+                                    return;
+                                }
+                                if (bIdentify)
+                                {
+                                    int ret = zkfp.ZKFP_ERR_OK;
+                                    int fid = 0, score = 0;
+                                    ret = zkfp2.DBIdentify(mDBHandle, CapTmp, ref fid, ref score);
+                                    if (zkfp.ZKFP_ERR_OK == ret)
+                                    {
+                                        textRes.Text = "Identify succ, fid= " + fid + ",score=" + score + "!";
+                                        return;
+                                    }
+                                    else
+                                    {
+                                        textRes.Text = "Identify fail, ret= " + ret;
+                                        return;
+                                    }
+                                }
+                                else
+                                {
+                                    int ret = zkfp2.DBMatch(mDBHandle, CapTmp, RegTmp);
+                                    if (0 < ret)
+                                    {
+                                        textRes.Text = "Match finger succ, score=" + ret + "!";
+                                        return;
+                                    }
+                                    else
+                                    {
+                                        textRes.Text = "Match finger fail, ret= " + ret;
+                                        return;
+                                    }
+                                }
+                            }
+                        }
+                        break;
+
+                    default:
+                        base.DefWndProc(ref m);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormHandle = this.Handle;
+        }
+
+        private void bnClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                bIsTimeToDie = true;
+                RegisterCount = 0;
+                Thread.Sleep(1000);
+                zkfp2.CloseDevice(mDevHandle);
+                bnInit.Enabled = false;
+                bnFree.Enabled = true;
+                bnOpen.Enabled = true;
+                bnClose.Enabled = false;
+                bnEnroll.Enabled = false;
+                bnVerify.Enabled = false;
+                bnIdentify.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
+        private void bnEnroll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!IsRegister)
+                {
+                    IsRegister = true;
+                    RegisterCount = 0;
+                    cbRegTmp = 0;
+                    textRes.Text = "Please press your finger 3 times!";
+                }
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
+        private void bnIdentify_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!bIdentify)
+                {
+                    bIdentify = true;
+                    textRes.Text = "Please press your finger!";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
+        private void bnVerify_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (bIdentify)
+                {
+                    bIdentify = false;
+                    textRes.Text = "Please press your finger!";
+                }
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+    }
+}
